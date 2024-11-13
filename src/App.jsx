77eb10b7ec897623a2a5e363d8116046e9786834
Shane Doyle <shanedoyle1989@gmail.com>
2024-11-13 16:29:23 +0000

    add Pizza component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import pizzaData from "./data";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Pizza />
+    </>
+  );
+}
+
+function Pizza() {
+  return (
+    <>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </>
+  );
 }
